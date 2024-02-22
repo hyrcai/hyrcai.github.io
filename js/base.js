9d509e7d3b36b43c556cf57b6bc86c66eb2076ef
Hannah <hannah.yanran.cai@gmail.com>
2024-02-21 18:28:08 -0800

    backdrop filter test

--- js/base.js
+++ js/base.js
@@ -3,14 +3,13 @@ $(function () {
     var includes = $('[data-include]')
     $.each(includes, function () {
         var file = '/include/' + $(this).data('include') + '.html'
-        $(this).load(file)
+        $(this).load(file);
     })
 })
 
 // hide loader on load
 $(window).on('load', function () {
-    // $('body>*:not([data-include="loader"])').css('opacity', '1');
-    $('body>*:not([data-include="loader"])').css('visibility', 'visible');
+    $('body>:not([data-include="loader"])').css('opacity', '1');
     $('#loader').addClass('hidden');
     setTimeout(function () {
         $('#loader').css('display', 'none');
