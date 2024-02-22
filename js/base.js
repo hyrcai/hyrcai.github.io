537ec3ef242274652fb49ae3982518ac8487755b
Hannah <hannah.yanran.cai@gmail.com>
2024-02-21 19:29:13 -0800

    update

--- js/base.js
+++ js/base.js
@@ -1,14 +1,5 @@
+// hide loader on load
 $(window).on('load', function () {
-    // load top and bottom nav
-    $(function () {
-        var includes = $('[data-include]')
-        $.each(includes, function () {
-            var file = '/include/' + $(this).data('include') + '.html';
-            $(this).load(file);
-        })
-    })
-
-    // hide loader
     $('body>:not([data-include="loader"])').css('opacity', '1');
     $('#loader').addClass('hidden');
     setTimeout(function () {
@@ -16,6 +7,15 @@ $(window).on('load', function () {
     }, 750);
     $('html').css('overflow-y', 'auto');
 });
+
+// load top and bottom nav
+$(function () {
+    var includes = $('[data-include]')
+    $.each(includes, function () {
+        var file = '/include/' + $(this).data('include') + '.html'
+        $(this).load(file);
+    })
+})
 
 //disable right click
 // document.addEventListener('contextmenu', event => event.preventDefault());
