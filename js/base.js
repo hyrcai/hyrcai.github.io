73a6b4e7534ce1197d8fd9fc667ef976a03e7905
Hannah <hannah.yanran.cai@gmail.com>
2024-02-23 12:52:42 -0800

    add set preload time

--- js/base.js
+++ js/base.js
@@ -9,12 +9,15 @@ $(function () {
 
 // hide loader on load
 $(window).on('load', function () {
-    $('body>:not([data-include="loader"])').css('opacity', '1');
-    $('#loader').addClass('hidden');
     setTimeout(function () {
-        $('#loader').css('display', 'none');
-    }, 750);
-    $('html').css('overflow-y', 'auto');
+        $('body>:not([data-include="loader"])').css('opacity', '1');
+        $('#loader').addClass('hidden');
+        setTimeout(function () {
+            $('#loader').css('display', 'none');
+        }, 750);
+        $('html').css('overflow-y', 'auto');
+    }, 3000);
+
 });
 
 //disable right click
