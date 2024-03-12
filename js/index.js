1ef676910b0e0ec09b9ce4480c2d261c6997ce97
Hannah <hannah.yanran.cai@gmail.com>
2024-03-12 15:44:39 -0700

    remove loader delay

--- js/index.js
+++ js/index.js
@@ -9,14 +9,14 @@ $(function () {
 
 // hide loader on load
 $(window).on('load', function () {
-    setTimeout(function () {
+    // setTimeout(function () {
         $('body>:not([data-include="loader"])').css('opacity', '1');
         $('#loader').addClass('hidden');
         setTimeout(function () {
             $('#loader').css('display', 'none');
         }, 750);
         $('html').css('overflow-y', 'auto');
-    }, 3000);
+    // }, 3000);
 
 });
 
